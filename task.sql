f886fe1db841b40f94dd3c1d4bc12e13b55c7044
AlexHCom <alexhcomgrit@gmail.com>
2024-02-08 16:42:11 +0200

    Solution

--- task.sql
+++ task.sql
@@ -2,7 +2,7 @@
 USE ShopDB; 
 
 INSERT INTO Orders (CustomerID, Date);
-VALUE ('1', '2023-01-01');
+VALUE (1, '2023-01-01');
 -- Start the transaction
 START TRANSACTION; 
 
@@ -10,12 +10,12 @@ SET @order_id = LAST_INSERT_ID();
 
 -- Add a product to the order
 INSERT INTO OrderItems (OrderID, ProductID, Count);
-VALUES ('1', '1', '1');
+VALUES (1, 1, 1);
 
 -- Update WarehouseAmount of the product
 UPDATE Products
 SET WarehouseAmount = WarehouseAmount - 1 WHERE Product.ID = (
-    SELECT ProductID FROM OrderItems WHERE OrderItems.OrderID = '1'
+    SELECT ProductID FROM OrderItems WHERE OrderItems.OrderID = 1
     );
 
 -- And some data should be created inside the transaction 
