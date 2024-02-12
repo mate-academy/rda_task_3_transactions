fddc13111a5e4b4612f7b52b8d94269210b8bfd7
Dmytry-S <sviridoffd@gmail.com>
2024-02-12 17:59:39 +0200

    Implement Use transaction task

--- task.sql
+++ task.sql
@@ -2,10 +2,15 @@
 USE ShopDB; 
 
 -- Some data should be created outside the transaction (here)
-
+INSERT INTO Orders (ID, CustomerID, Date)
+	VALUES (1, 1, '2024-01-31');
+	
+INSERT INTO OrderItems (ID, OrderID, ProductID, Count)
+	VALUES (1, 1, 1, 1);
 -- Start the transaction 
 START TRANSACTION; 
 
 -- And some data should be created inside the transaction 
-
+UPDATE Products 
+SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
 COMMIT; 
