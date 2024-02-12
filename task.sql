d747a14400a68af4574076da7a1046e7d903533c
Dmytry-S <sviridoffd@gmail.com>
2024-02-12 18:04:01 +0200

    Update Use transaction task

--- task.sql
+++ task.sql
@@ -5,11 +5,11 @@ USE ShopDB;
 INSERT INTO Orders (ID, CustomerID, Date)
 	VALUES (1, 1, '2024-01-31');
 	
-INSERT INTO OrderItems (ID, OrderID, ProductID, Count)
-	VALUES (1, 1, 1, 1);
+
 -- Start the transaction 
 START TRANSACTION; 
-
+INSERT INTO OrderItems (ID, OrderID, ProductID, Count)
+	VALUES (1, 1, 1, 1);
 -- And some data should be created inside the transaction 
 UPDATE Products 
 SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
