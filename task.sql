097359e05f6817593523a7c4ea00a5085cc0f76b
Dmytry-S <sviridoffd@gmail.com>
2024-02-13 08:21:31 +0200

    Delete IDs from querys

--- task.sql
+++ task.sql
@@ -2,14 +2,14 @@
 USE ShopDB; 
 
 -- Some data should be created outside the transaction (here)
-INSERT INTO Orders (ID, CustomerID, Date)
-	VALUES (1, 1, '2024-01-31');
+INSERT INTO Orders (CustomerID, Date)
+	VALUES (1, '2024-01-31');
 	
 
 -- Start the transaction 
 START TRANSACTION; 
-INSERT INTO OrderItems (ID, OrderID, ProductID, Count)
-	VALUES (1, 1, 1, 1);
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+	VALUES (1, 1, 1);
 -- And some data should be created inside the transaction 
 UPDATE Products 
 SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
