0996569e373e97b9e9c70f0850e2f83741cd9ffe
Yaroslav Yavornytskyi <ya.yaroslav27@gmail.com>
2024-03-04 15:55:08 +0100

    Solution

--- task.sql
+++ task.sql
@@ -2,12 +2,15 @@
 USE ShopDB; 
 
 -- Some data should be created outside the transaction (here)
-INSERT INTO Orders (ID, CustomerID, Date) VALUES ('1', '1', '2023-01-01');
-INSERT INTO OrderItems (ID, OrderID, ProductID, Count) VALUES ('1', '1', '1', '1');
-
+INSERT INTO Orders (CustomerID, Date)
+VALUES (1, '2024-02-06');
 -- Start the transaction 
 START TRANSACTION;
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+VALUES (1, 1, 1);
 
-UPDATE Products SET WarehouseAmount = 41 WHERE ID = '1';
+UPDATE Products
+SET WarehouseAmount = WarehouseAmount - 1
+WHERE Products.ID = 1;
 
-COMMIT; 
+COMMIT;
